e1091d73f06f02b087685b405409e7c9510e1839
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-06-26 02:21:46 -0400

    Fixed wrong sliding

--- app/static/scripts/hobbies.js
+++ app/static/scripts/hobbies.js
@@ -1,13 +1,13 @@
 setInterval(controlSlider, 5000);
 
-const slider = document.querySelector(".wrapper");
-const sliderItems = document.querySelectorAll(".hobbyWrapper");
-const sliderLength = sliderItems.length;
-const sliderWidth = sliderItems[0].clientWidth;
-
 let index = 0;
 
 function controlSlider() {
+  const slider = document.querySelector(".wrapper");
+  const sliderItems = document.querySelectorAll(".hobbyWrapper");
+  const sliderLength = sliderItems.length;
+  const sliderWidth = sliderItems[0].clientWidth;
+
   index++;
   if (index === sliderLength) {
     index = 0;
