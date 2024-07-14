3b1c5d684fc0b1621fbfc0f870ca8211a0db3a73
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-07-14 14:36:48 -0400

    Updated redeploy.sh with services instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,11 +2,11 @@
 
 PROJECT_DIR="/root/lms-portfolio"
 
-tmux kill-session -t portfolio
 cd $PROJECT_DIR
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s portfolio "cd $PROJECT_DIR && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl deamon-reload
+systemctl restart myportfolio
