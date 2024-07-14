c233555366acc0e2df06779a481dc39f22093c00
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-07-14 14:39:49 -0400

    fixed redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,5 +8,5 @@ python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-systemctl deamon-reload
+systemctl daemon-reload
 systemctl restart myportfolio
