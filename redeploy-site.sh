a4f503c0526935827eda16ef36ee0b41b24a283a
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-07-28 19:38:09 -0400

    updated redeploy-site.sh file

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,9 +4,5 @@ PROJECT_DIR="/root/lms-portfolio"
 
 cd $PROJECT_DIR
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
